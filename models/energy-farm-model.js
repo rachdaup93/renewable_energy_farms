e5d073facce3f8226919e2dd51aa5e63bffaf55c
saraisatiger <shashem0221@gmail.com>
2019-06-06 12:31:47 -0400

    Add unit capacity attribute to model schema

--- models/energy-farm-model.js
+++ models/energy-farm-model.js
@@ -2,16 +2,16 @@ const mongoose   = require('mongoose');
 const Schema     = mongoose.Schema;
 const energyFarmSchema = new Schema({
     name: { type: String, required: true },
-    renewable_type: { type: String, required: true },
+    renewableType: { type: String, required: true },
     location: { type: String, required: true },
     email: { type: String, required: true },
-    weather_quality: { type: Number, require: true },
-    weather_quality_type: { type: String, require: true },
-    total_units: { type: Number, require: true },
+    weatherQuality: { type: Number, require: true },
+    weatherQualityType: { type: String, require: true },
+    totalUnits: { type: Number, require: true },
     operating_units: { type: Number, require: true },
     condition: { type: String, require: true },
     deployment_required: { type: Boolean, require: false },
-    service_required: { type: Boolean, require: false },
+    service_required: { type: Boolean, require: false },r
     unit_capacity: { type: Number, require: true },
   },
   // optional settings object for this schema
