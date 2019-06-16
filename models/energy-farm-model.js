194c09a29921c64b9adfd0d25326573a8630030e
Rachelle Dauphin <rachdaup.tech@gmail.com>
2019-06-16 03:42:54 -0400

    Add images. Rename example css and html files.

--- models/energy-farm-model.js
+++ models/energy-farm-model.js
@@ -4,14 +4,13 @@ const energyFarmSchema = new Schema({
     name: { type: String, required: true },
     renewableType: { type: String, required: true },
     location: { type: String, required: true },
-    email: { type: String, required: true },
     weatherQuality: { type: Number, require: true },
     weatherQualityType: { type: String, require: true },
     totalUnits: { type: Number, require: true },
     operating_units: { type: Number, require: true },
     condition: { type: String, require: true },
     deployment_required: { type: Boolean, require: false },
-    service_required: { type: Boolean, require: false },r
+    service_required: { type: Boolean, require: false },
     unit_capacity: { type: Number, require: true },
   },
   // optional settings object for this schema
