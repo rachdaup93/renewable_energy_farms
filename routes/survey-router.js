af64b4de48ac15aa457734aed05fa41dc593ce19
rxd0w2h <rachelle.dauphin@nexteraenergy.com>
2019-11-11 10:59:19 -0500

    Remove wind folder.

--- routes/survey-router.js
+++ routes/survey-router.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router  = express.Router();
+const fs = require('fs');
 
 router.get('/:team', (req, res, next) => {
   // check for feedback messages from the sign up process
-  let teamName = req.params.team;
+  const teamName = req.params.team;
+  const customScript = `'../public/_js/surveys/'${ teamName }.js`;
+
   res.locals.stylesheet = `/_css/surveys/${ teamName }.css`;
   res.locals.script = 'formValidation.js';
+
+  if (fs.existsSync(customScript)) {
+    res.locals.script2 = `/_js/surveys/${ teamName }.js`;
+  }
+  
   res.render(`surveys/${ teamName }`);
 });
 
